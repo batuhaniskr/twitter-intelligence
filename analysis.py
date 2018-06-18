5a32f95c9f98a110b388d9727a3d658fbd5ae2e1
Batuhan Işıker <bthnskr@gmail.com>
2018-06-18 10:51:56 +0300

    Added locations analysis.

--- analysis.py
+++ analysis.py
@@ -1,19 +1,23 @@
+# -*- encoding: utf-8 -*-
+
 import matplotlib.pyplot as pl
 from collections import Counter
-import sqlite3, os
+import sqlite3
 import os.path
 import numpy as np
+from geopy.geocoders import Nominatim
+from flask import Flask, render_template
+import json
+
+ROOT_DIR = os.path.dirname(os.pardir)
+db_path = os.path.join(ROOT_DIR, "TweetAnalysis.db")
 
 def analysis_graph():
-    ROOT_DIR = os.path.dirname(os.pardir)
-    db_path = os.path.join(ROOT_DIR, "TweetAnalysis.db")
 
     with sqlite3.connect(db_path) as db:
 
         conn = db
         c = conn.cursor()
-
-        #username - tweet analysis
         c.execute("SELECT  username, count(*) as tekrar FROM Tweet  group by username order by tekrar desc LIMIT 10")
         data = c.fetchall()
         ilk=[]
@@ -34,28 +38,22 @@ def analysis_graph():
         pl.ylabel('Tweet Count')
         pl.show()
 
-        #Hashtag Analysis
         c.execute("SELECT hashtag from Tweet")
         hashtag_list = []
         for row in c.fetchall():
             if " " in ''.join(row):
-                for m in ''.join(i).split(' '):
+                for m in ''.join(row).split(' '):
                     hashtag_list.append(m)
             else:
                 signle_item  = ''.join(row)
                 hashtag_list.append(signle_item)
 
-        print(Counter(hashtag_list))
         counter = Counter(hashtag_list)
 
         pl.rcdefaults()
-        # Counter data, counter is your counter object
         keys = counter.keys()
         y_pos = np.arange(len(keys))
-        # get the counts for each key, assuming the values are numerical
         performance = [counter[k] for k in keys]
-        print(performance)
-        # not sure if you want this :S
         error = np.random.rand(len(keys))
 
         pl.barh(y_pos, performance, xerr=error, align='center', alpha=0.4, )
@@ -66,3 +64,44 @@ def analysis_graph():
 
 analysis_graph()
 
+def location_analysis():
+    with sqlite3.connect(db_path) as db:
+        conn = db
+        c = conn.cursor()
+
+        locxy = []
+        c.execute("Select place from location")
+        loc_array = c.fetchall()
+
+        # mapping
+        geo_data = {
+
+            "features": []
+        }
+        for x in range(len(loc_array)):
+            if (loc_array[x] != ''):
+                geolocator = Nominatim()
+                location = geolocator.geocode(loc_array[x])
+                locxy.append(location.latitude)
+                locxy.append(location.longitude)
+
+                geo_json_feature = {
+                    "lat": location.latitude,
+                    "lng": location.longitude
+                }
+
+                geo_data['features'].append(geo_json_feature)
+                locxy.clear()
+        json_location = json.dumps(geo_data)
+        return json_location
+
+app = Flask(__name__)
+
+@app.route('/locations')
+def map():
+   location = location_analysis()
+
+   return render_template('locations.html', location = location)
+
+if __name__ == '__main__':
+    app.run()
